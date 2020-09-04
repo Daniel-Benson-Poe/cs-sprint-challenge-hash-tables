0166107c2dc531fdf5c931a42934fa3e1d6e35e7
Poenomaly <djbenson741@gmail.com>
2020-09-04 11:16:36 -0600

    Complete ex4 MVP

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,21 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    hash = {}
+    negatives = []
+    positives = []
+    for val in a:
+        if val < 0:
+            negatives.append(abs(val))
+        if val > 0:
+            positives.append(val)
 
+    result = []
+
+    for val in positives:
+        if val in negatives:
+            result.append(val)
+            
     return result
 
 
